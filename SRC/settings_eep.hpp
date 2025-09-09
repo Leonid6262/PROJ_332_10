4921563301daba6a85d302366475f5ae780ce5e3
Leonid6262 <leonid.ovchinnikov.62@gmail.com>
2025-09-09 15:56:15 +0300

    В settings_eep добавлена структура коэффициентов пересчёта в СИ

--- SRC/settings_eep.hpp
+++ SRC/settings_eep.hpp
@@ -16,17 +16,25 @@ class CEEPSettings {
 private:
   // --- Структура уставок ---
   #pragma pack(push, 2) // Выравнивание по 2 байта
-  struct WorkSettings {
+  struct WorkSettings 
+  {
     unsigned short checkSum;                            // 0 Контрольная сумма
     unsigned short SNboard_date;                        // 1 Серийный номер платы - дата (ст.б - месяц, мл.б - год)
     unsigned short SNboard_number;                      // 2 Серийный номер платы - порядковый номер (99.99.0999)
     signed short shift_adc[G_CONST::NUMBER_CHANNELS];   // 3 Смещения АЦП 
     float incline_adc[G_CONST::NUMBER_CHANNELS];        // 4 Наклон
     signed short shift_dac0;                            // 5 Смещение DAC0
-    unsigned char din_Pi0_invert;                       // 6 Признак инвертирования входов порта Pi0
-    unsigned char din_Pi1_invert;                       // 7 Признак инвертирования входов порта Pi1
-    unsigned char din_spi_invert[G_CONST::BYTES_RW];    // 8 Признак инвертирования SPI входов 
-    unsigned char dout_spi_invert[G_CONST::BYTES_RW];   // 9 Признак инвертирования SPI выходов
+    struct Disp_c                                       // 6 Коэффициенты для отображения в системе (СИ)
+    {
+      float C_ROTOR_CURRENT;
+      float C_STATOR_CURRENT;
+      float C_ROTOR_VOLTAGE;
+      float C_STATOR_VOLTAGE;      
+    } disp_c_settings;
+    unsigned char din_Pi0_invert;                       // 7 Признак инвертирования входов порта Pi0
+    unsigned char din_Pi1_invert;                       // 8 Признак инвертирования входов порта Pi1
+    unsigned char din_spi_invert[G_CONST::BYTES_RW];    // 9 Признак инвертирования SPI входов 
+    unsigned char dout_spi_invert[G_CONST::BYTES_RW];   //10 Признак инвертирования SPI выходов
     
     // Добавляя новые уставки сюда, не забывайте обновлять defaultSettings ниже!!!
   };
@@ -34,7 +42,8 @@ private:
   //  Статические константные уставки по умолчанию (во Flash) ---
   // 'static const inline' позволяет определить ее прямо здесь, в .h файле.
   #pragma pack(push, 2) // Выравнивание по 2 байта
-  static const inline WorkSettings defaultSettings {
+  static const inline WorkSettings defaultSettings 
+  {
     .checkSum = 0x0000, 
     .SNboard_date = 0x6363,
     .SNboard_number = 999,
@@ -49,6 +58,12 @@ private:
       0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f
     },
     .shift_dac0 = 0,
+    .disp_c_settings = {
+      .C_ROTOR_CURRENT  = 1.0f,
+      .C_STATOR_CURRENT = 1.0f,
+      .C_ROTOR_VOLTAGE  = 1.0f,
+      .C_STATOR_VOLTAGE = 1.0f
+    },
     .din_Pi0_invert = 0,
     .din_Pi1_invert = 0,
     .din_spi_invert =  { 0,0,0 },
